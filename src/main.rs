251a5b689acbf008ada5b1693d12861a4e94aafc
Patrik M. Rosenström <patrik.millvik@gmail.com>
2020-11-22 04:13:27 +0100

    Working on a memory map and a simple allocator.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
+#![feature(panic_info_message, alloc_error_handler)]
+
 #![no_std]
 #![no_main]
 
 extern crate rlibc;
+#[macro_use] extern crate alloc;
 
 use core::panic::PanicInfo;
 
+use alloc::alloc::{GlobalAlloc, Layout};
+use alloc::boxed::Box;
+
 #[repr(C)]
 struct SimpleTextOutputInterface {
-    reset: u64,
+    reset_fn: usize,
 
     output_string_fn: unsafe fn(&SimpleTextOutputInterface, *const u16) -> u64,
-    test_string: u64,
+    test_string_fn: usize,
     
-    quary_mode: u64,
-    set_mode: u64,
-    set_attribute: u64,
+    quary_mode_fn: usize,
+    set_mode_fn: usize,
+    set_attribute_fn: usize,
 
     clear_screen_fn: unsafe fn(&SimpleTextOutputInterface) -> u64,
-    set_cursor_position: u64,
-    enable_cursor: u64,
+    set_cursor_position_fn: usize,
+    enable_cursor_fn: usize,
 
-    mode: u64,
+    mode_fn: usize,
 }
 
 impl SimpleTextOutputInterface {
@@ -34,7 +40,8 @@ impl SimpleTextOutputInterface {
 }
 
 #[repr(C)]
-struct SystemTableHeader {
+#[derive(Clone, Copy, Debug)]
+struct TableHeader {
     signature: u64,
     revision: u32,
     header_size: u32,
@@ -42,27 +49,120 @@ struct SystemTableHeader {
     reserved: u32,
 }
 
-#[repr(C)]
-struct SystemTable<'a> {
-    header: SystemTableHeader,
+#[derive(Clone, Copy, Debug)]
+struct PhysicalAddress(usize);
 
-    firmware_vendor: u64,
+#[derive(Clone, Copy, Debug)]
+struct VirtualAddress(usize);
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+struct MemoryDescriptor {
+    typ: u32,
+    pad: u32,
+    physical_start: PhysicalAddress,
+    virtual_start: VirtualAddress,
+    number_of_pages: u64,
+    attribute: u64,
+}
+
+impl MemoryDescriptor {
+    fn empty() -> Self {
+        Self {
+            typ: 0,
+            pad: 0,
+            physical_start: PhysicalAddress(0),
+            virtual_start: VirtualAddress(0),
+            number_of_pages: 0,
+            attribute: 0,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct BootServices {
+    header: TableHeader,
+
+    raise_tpl_fn: usize,
+    restore_tpl_fn: usize,
+
+    allocate_pages_fn: usize,
+    free_pages_fn: usize,
+    get_memory_map_fn: unsafe fn(&mut u64, *mut MemoryDescriptor, 
+                                 &mut u64, &mut u64, &mut u32) -> u64,
+    allocate_pool_fn: unsafe fn(u32, u64, &mut *mut u8),
+    free_pool_fn: unsafe fn(*mut u8),
+
+    create_event_fn: usize,
+    set_timer_fn: usize,
+    wait_for_event_fn: usize,
+    signal_event_fn: usize,
+    close_event_fn: usize,
+    check_event_fn: usize,
+
+    install_protocol_interface_fn: usize,
+    reinstall_protocol_interface_fn: usize,
+    uninstall_protocol_interface_fn: usize,
+    handle_protocol_fn: usize,
+    pc_handle_protocol_fn: usize,
+    register_protocol_notify_fn: usize,
+    locate_handle_fn: usize,
+    locate_device_path_fn: usize,
+    install_configuration_table_fn: usize,
+
+    load_image_fn: usize,
+    start_image_fn: usize,
+    exit_fn: usize,
+    unload_image_fn: usize,
+    exit_boot_services_fn: usize,
+
+    get_next_monotonic_count_fn: usize,
+    stall_fn: usize,
+    set_watchdog_timer_fn: usize,
+
+    connect_controller_fn: usize,
+    disconnect_controller_fn: usize,
+
+    open_protocol_fn: usize,
+    close_protocol_fn: usize,
+    open_protocol_infomation_fn: usize,
+
+    protocols_per_handle_fn: usize,
+    locate_handle_buffer_fn: usize,
+    locate_protocol_fn: usize,
+    install_multiple_protocol_interfaces_fn: usize,
+    uninstall_multiple_protocol_interfaces_fn: usize,
+
+    calculate_crc32_fn: usize,
+
+    copy_mem_fn: usize,
+    set_mem_fn: usize,
+    create_event_ex_fn: usize,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct SystemTable<'a> {
+    header: TableHeader,
+
+    firmware_vendor: usize,
     firmware_revision: u32,
 
-    console_in_handle: u64,
-    con_in: u64,
+    console_in_handle: usize,
+    con_in: usize,
 
-    console_out_handle: u64,
+    console_out_handle: usize,
     console_out: &'a SimpleTextOutputInterface,
 
-    standard_error_handle: u64,
-    stderr: u64,
+    standard_error_handle: usize,
+    stderr: &'a SimpleTextOutputInterface,
 
-    runtime_services: u64,
-    boot_services: u64,
+    runtime_services: usize,
+    boot_services: &'a BootServices,
 
     number_of_table_entries: u64,
-    configuration_table: u64,
+    configuration_table: usize,
 }
 
 struct TextWriter<'a> {
@@ -118,6 +218,7 @@ impl<'a> core::fmt::Write for TextWriter<'a> {
 }
 
 static mut WRITER: Option<TextWriter> = None;
+static mut TABLE: Option<SystemTable<'static>> = None;
 
 macro_rules! print {
     ($($arg:tt)*) => ({
@@ -136,6 +237,28 @@ macro_rules! println {
     ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
 }
 
+
+struct EFIAllocator;
+
+unsafe impl GlobalAlloc for EFIAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let mut buffer = core::ptr::null_mut();
+        (TABLE.unwrap().boot_services.allocate_pool_fn)(4, layout.size() as u64, &mut buffer);
+
+        buffer
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+}
+
+#[global_allocator]
+static A: EFIAllocator = EFIAllocator;
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
+}
+
 #[no_mangle]
 fn efi_main(_image_handle: u64, 
             system_table: *const SystemTable<'static>) 
@@ -145,24 +268,54 @@ fn efi_main(_image_handle: u64,
 
     unsafe {
         table.console_out.clear_screen();
-        // (table.console_out.clear_screen)(table.console_out);
     }
 
     unsafe {
         WRITER = Some(TextWriter::new(table.console_out));
+        TABLE = Some(*table);
     }
 
+    let mut map_size = 0;
+    let mut map_key = 0;
+    let mut entry_size = 0;
+    let mut entry_version = 0;
+
+    let status = unsafe {
+        (table.boot_services.get_memory_map_fn)(
+            &mut map_size,
+            core::ptr::null_mut(),
+            &mut map_key,
+            &mut entry_size,
+            &mut entry_version,
+        )
+    };
+
+    let test = vec![123, 321, 123];
+
     println!("Welcome to the potato bootloader v0.1");
+    println!("Boot Services Size: {}", core::mem::size_of::<BootServices>());
+    println!("Test: {:?}", test);
 
     // TODO(patrik): Load the kernel
 
     loop {}
-        
-    0
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    print!("PANIC");
+fn panic(info: &PanicInfo) -> ! {
+    println!("---------- KERNEL PANIC ----------");
+    
+    if let Some(msg) = info.message() {
+        println!("Message: {}", msg);
+    }
+
+    if let Some(loc) = info.location() {
+        println!("Location: {}:{}:{}", 
+                 loc.file(), loc.line(), loc.column());
+    }
+
+
+    println!("----------------------------------");
+
     loop {}
 }
